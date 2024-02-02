4bd69bc872052efbd88617dc93db0269c4052672
abdelrahmankamel20 <abdelrahman.elgendy.333@gmail.com>
2024-02-03 00:45:30 +0200

    first commit

---
+++ E-Ticket/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Ticket.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Required, Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ E-Ticket/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+using E_Ticket.Data.Enums;
+using E_Ticket.Data.Base;
+
+namespace E_Ticket.Data.ViewModels
+{
+    public class NewMovieVM 
+    {
+        [Required, Display(Name = "Movie name")]
+        public string Name { get; set; }
+
+        [Required, Display(Name = "Movie description")]
+        public string Description { get; set; }
+
+        [Required, Display(Name = "Price")]
+        public double Price { get; set; }
+
+        [Required, Display(Name = "Movie poster URL")]
+        public string Poster { get; set; }
+
+        [Required, Display(Name = "Movie start date")]
+        public DateTime StartDate { get; set; }
+
+        [Required, Display(Name = "Movie end date")]
+        public DateTime EndDate { get; set; }
+
+        [Required, Display(Name = "Select a category")]
+        public Category Category { get; set; }
+
+        //Relationships
+        [Required, Display(Name = "Select actors")]
+        public List<int> ActorIds { get; set; }
+
+        [Required, Display(Name = "Select a cinema")]
+        public int CinemaId { get; set; }
+
+        [Required, Display(Name = "Select a producer")]
+        public int ProducerId { get; set; }
+    }
+}
